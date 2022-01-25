4e8352f8b03390766d7efa10923681dda875d227
Isaac Souza <isaacsouza17@gmail.com>
2022-01-25 08:00:05 -0300

    Middleware to simulate real world network delay

---
+++ app/Http/Middleware/Delay.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Delay
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  string|null  ...$guards
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, ...$guards)
+    {
+        if(config('app.env') != 'production')
+        {
+            sleep(1);
+        }
+
+        return $next($request);
+    }
+}
