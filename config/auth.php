83803a0fbfc6996e33696c9cea1c279f79bdd13c
Isaac Souza <isaacsouza17@gmail.com>
2022-01-25 07:59:01 -0300

    Removed jwt configs

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'api',
+        'guard' => 'web',
         'passwords' => 'users',
     ],
 
@@ -40,10 +40,6 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'api' => [
-            'driver' => 'jwt',
-            'provider' => 'users',
-        ]
     ],
 
     /*
