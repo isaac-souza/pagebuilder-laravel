dd6a132fc64cbd6ec763714ef6ccbc326f486957
Isaac Souza <isaacsouza17@gmail.com>
2022-02-06 16:34:11 -0300

    Code formatting

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -9,22 +9,22 @@ use App\Http\Controllers\AuthController;
 
 Route::prefix('public')->group(function() {
 
-    Route::get('landing-pages/{slug}', [PublicLandingPageController::class, 'show'])->name('public.landing-pages.show');
+    Route::get('landing-pages/{slug}',          [PublicLandingPageController::class, 'show'])->name('public.landing-pages.show');
 
 });
 
 Route::middleware(['simulate.network.delay', 'auth:sanctum'])->group(function() {
 
-    Route::get('/auth/check', [AuthController::class, 'check']);
-    Route::get('/auth/account', [AuthController::class, 'account']);
+    Route::get('/auth/check',                   [AuthController::class, 'check']);
+    Route::get('/auth/account',                 [AuthController::class, 'account']);
 
-    Route::put('landing-pages/{uuid}/draft', [LandingPageDraftController::class, 'update']);
+    Route::put('landing-pages/{uuid}/draft',    [LandingPageDraftController::class, 'update']);
     
-    Route::get('landing-pages',             [LandingPageController::class, 'index'])->name('landing-pages.index');
-    Route::get('landing-pages/{uuid}',      [LandingPageController::class, 'show'])->name('landing-pages.show');
-    Route::post('landing-pages',            [LandingPageController::class, 'store'])->name('landing-pages.store');
-    Route::put('landing-pages/{uuid}',      [LandingPageController::class, 'update'])->name('landing-pages.update');
-    Route::delete('landing-pages/{uuid}',   [LandingPageController::class, 'destroy'])->name('landing-pages.destroy');
+    Route::get('landing-pages',                 [LandingPageController::class, 'index'])->name('landing-pages.index');
+    Route::get('landing-pages/{uuid}',          [LandingPageController::class, 'show'])->name('landing-pages.show');
+    Route::post('landing-pages',                [LandingPageController::class, 'store'])->name('landing-pages.store');
+    Route::put('landing-pages/{uuid}',          [LandingPageController::class, 'update'])->name('landing-pages.update');
+    Route::delete('landing-pages/{uuid}',       [LandingPageController::class, 'destroy'])->name('landing-pages.destroy');
 
     Route::prefix('images')->group(function() {
 
